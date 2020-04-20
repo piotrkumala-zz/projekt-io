26ba700dc3dcf85971aeacf73bd6724b7bb3a7bf
Maciej Domagalski <ikslamabad@gmail.com>
2020-04-20 18:43:45 +0200

    feature: authentication (I guess?)

---
+++ server/user/UserController.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var VerifyToken = require('../auth/VerifyToken');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+var User = require('./User');
+
+// CREATES A NEW USER
+router.post('/', function (req, res) {
+    User.create({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password
+        }, 
+        function (err, user) {
+            if (err) return res.status(500).send("Nie udało się dodać użytkownika.");
+            res.status(200).send(user);
+        });
+});
+
+// RETURNS ALL THE USERS IN THE DATABASE
+router.get('/', function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) return res.status(500).send("Nie udało się wyszukać.");
+        res.status(200).send(users);
+    });
+});
+
+// GETS A SINGLE USER FROM THE DATABASE
+router.get('/:id', function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("Nie udało się wyszukać.");
+        if (!user) return res.status(404).send("Nie ma takiego użytkownika.");
+        res.status(200).send(user);
+    });
+});
+
+// DELETES A USER FROM THE DATABASE
+router.delete('/:id', function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("Nie udało się usunąć.");
+        res.status(200).send("Udało się usunąć użytkownika: "+ user.name);
+    });
+});
+
+// UPDATES A SINGLE USER IN THE DATABASE
+// Added VerifyToken middleware to make sure only an authenticated user can put to this route
+router.put('/:id', /* VerifyToken, */ function (req, res) {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+        if (err) return res.status(500).send("Nie udało się zmienić danych.");
+        res.status(200).send(user);
+    });
+});
+
+
+module.exports = router;
