4ce1e65b554ce42b7bd2cbcf4ce5c737fbeba7a6
Piotr <piotrek.kumala@gmail.com>
2020-05-10 18:38:00 +0000

    Change food connection from local to cloud

--- server/routes/food.js
+++ server/routes/food.js
@@ -3,11 +3,12 @@ var express = require('express');
 var router = express.Router();
 const conString = process.env.ELEPHANTSQL_URL || "postgres://nviwmkcg:4-G6tFZ1xeDNz_W6f9C-qWQTJo8sq6Ww@drona.db.elephantsql.com:5432/nviwmkcg";
 const pool = new pg.Pool({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database:'postgres',
-    password: 'postgres',
-    port: 5432
+    user: 'nviwmkcg',
+    host: 'drona.db.elephantsql.com',
+    database:'nviwmkcg',
+    password: '4-G6tFZ1xeDNz_W6f9C-qWQTJo8sq6Ww',
+    port: 5432,
+    ssl: true
 })
 
 
