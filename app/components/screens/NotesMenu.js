dc927c097cce5003f6e2c1b6adced3409ce04bb0
T0Mage <tompraszkiewicz@gmail.com>
2020-06-01 15:48:18 +0200

    added ability to add notes

--- app/components/screens/NotesMenu.js
+++ app/components/screens/NotesMenu.js
@@ -1,12 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 
 function GetData(props) {
     const [data, setData] = useState(null);
     const [listItems, setlistItems] = useState(null);
+  //  const [nr, setNr] = useState(null);
+    const [text, setText] = useState(null);
+    const NotePressed = async () => {
+        console.log("UWAGA! Zapalono papieroska");
+        const data1 = {
+            email: 'adam@gmail.com',
+            type: 'd',
+            nr: parseInt(Math.random()*10000000+1000000),
+            text: text
+        };
+        console.log(data1);
+        const res = await fetch('http://192.168.178.200:3000/note/add', {
+            method: 'POST',
+            body: JSON.stringify(data1),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const getData = async () => {
+            const res = await fetch('http://192.168.178.200:3000/note?email=adam@gmail.com');
+            const data = await res.json();
 
+            setData(data)
+        }
+        getData();
+        setlistItems(data.map((nnote) => <Text key={nnote["nr_notatki"] + "" + nnote["rodzaj"]}>{nnote["tekst"]}{"\n"}</Text>));
+
+    }
     useEffect(() => {
         const getData = async () => {
             const res = await fetch('http://192.168.178.200:3000/note?email=adam@gmail.com');
@@ -19,16 +45,28 @@ function GetData(props) {
 
     if (data != null) {
         if (listItems == null) {
-        setlistItems (data.map((nnote) => <Text key={nnote["nr_notatki"]+""+nnote["rodzaj"]}>{nnote["tekst"]}{"\n"}</Text>));
+            setlistItems(data.map((nnote) => <Text key={nnote["nr_notatki"] + "" + nnote["rodzaj"]}>{nnote["tekst"]}{"\n"}</Text>));
+   //         setNr(data.slice(-1)[0]["nr"] + 100);
         }
-      //  console.log(data);
-       // console.log(listItems);
+        //  console.log(data);
+        // console.log(listItems);
 
         return (
             <View>
                 {listItems}
                 <Text>Koniec Notatek</Text>
 
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    multiline={true}
+                    numberOfLines={4}
+                    onChangeText={(text) => setText(text)}
+                    value={text} />
+                <TouchableOpacity
+                    onPress={NotePressed}
+                    style={styles.button}>
+                    <Text>send</Text>
+                </TouchableOpacity>
             </View>
 
         )
@@ -39,19 +77,14 @@ function GetData(props) {
 
 const NotesMenu = props => {
 
-    const [text, setText] = useState(null);
 
-
+  
 
     return (
         <View>
             <GetData></GetData>
-            <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                multiline={true}
-                numberOfLines={4}
-                onChangeText={(text) => setText(text)}
-                value={text} />
+
+
         </View>
     )
 }
