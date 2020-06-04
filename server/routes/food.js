0afd4181d769e959c146eb56681fafb938bf9ea1
Piotr Kumala <piotrek.kumala@gmail.com>
2020-06-04 00:01:53 +0000

    Fix error style on AddProduct

--- server/routes/food.js
+++ server/routes/food.js
@@ -41,8 +41,7 @@ router.post('/delete', async (req, res, next)=>{
 })
 
 router.post('/add', async (req, res, next)=>{
-    console.log(req.body)
-    if(Object.values(req.body).any(x => x == null))
+    if(Object.values(req.body).every(x => x != null))
         res.json({
             error: true,
             message: 'body is empty'
