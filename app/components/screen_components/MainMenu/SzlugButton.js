6ce542efeb1c9736e8d7fc84df03b206d7f38db5
T0Mage <tompraszkiewicz@gmail.com>
2020-06-05 10:41:27 +0200

    added dropdown menu for smoke button

--- app/components/screen_components/MainMenu/SzlugButton.js
+++ app/components/screen_components/MainMenu/SzlugButton.js
@@ -1,4 +1,6 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+import { Dropdown } from 'react-native-material-dropdown';
 import {
 	View,
 	TouchableOpacity,
@@ -7,14 +9,31 @@ import {
 } from 'react-native';
 
 const SzlugButton = () => {
+	const [typ, setTyp] = useState('Normalny');
+	data = ([
+		{
+			value: 'Normalny'
+		},{
+			value: 'Mocny'
+		}, {
+			value: 'Light'
+		},{
+			value: 'Ultra Light'
+		}
+	]);
+
+
+	console
 	const szlugPressed = async () => {
 		console.log("UWAGA! Zapalono papieroska");
+		console.log(data);
+		
 		const data1 = {
 			email: 'adam@gmail.com',
 			day: new Date(Date.now()).toISOString().replace('T', ' ').replace('Z', ''),
 			count: 1,
 			price: 1.1,
-			type: 'normal',
+			type: typ,
 		};
 		console.log(data1);
 		const res = await fetch('http://192.168.178.200:3000/smoke/add', {
@@ -25,9 +44,23 @@ const SzlugButton = () => {
 			}
 		});
 	}
-
+	function handleRodzaj(e) {
+//		console.log(e);
+		setTyp(e);
+	}
 	return (
 		<View style={styles.view}>
+
+			<Dropdown
+				value="Normalny"
+				onChangeText={handleRodzaj}
+				label="Rodzaj"
+				data={data}
+				
+
+			/>
+
+
 			<TouchableOpacity
 				onPress={szlugPressed}
 				style={styles.button}>
