89c3e45b7feab29bc1185e409b1cc271b807d9ad
T0Mage <tompraszkiewicz@gmail.com>
2020-06-05 11:12:39 +0200

    added stats 30 7 1 days

--- app/components/screens/SmokingStats.js
+++ app/components/screens/SmokingStats.js
@@ -5,27 +5,46 @@ import { StyleSheet, Text, View } from 'react-native';
 
 function GetData(props) {
     const [data, setData] = useState(null);
+    const [data7, setData7] = useState(null);
+    const [data30, setData30] = useState(null);
 
     useEffect(() => {
         const getData = async () => {
-            const res = await fetch('http://192.168.178.200:3000/smoke/sum?email=adam@gmail.com&days=30');
+            const res30 = await fetch('http://192.168.178.200:3000/smoke/sum?email=adam@gmail.com&days=30');
+            const data30 = await res30.json();
+            const res7 = await fetch('http://192.168.178.200:3000/smoke/sum?email=adam@gmail.com&days=7');
+            const data7 = await res7.json();
+            const res = await fetch('http://192.168.178.200:3000/smoke/sum?email=adam@gmail.com&days=1');
             const data = await res.json();
-
+            console.log(data30);
+            
             setData(data)
+            setData7(data7)
+            setData30(data30)
         }
         getData();
     }, [])
-    if (data != null) {
+    if (data != null&&data7 != null&&data30 != null) {
         return (
             <View>
                 < Text >
                     Użytkownik: {data[0]["email"]}
                 </Text >
                 < Text >
-                    Wydałeś przez ostatnie 30 dni {data[0]["sum"]} zl na papierosy
+                    Wydałeś przez ostatnio:
                 </Text >
-
-
+                <Text>
+                    30 dni: {data30[0]["sum"]}
+                </Text>
+                <Text>
+                    7 dni: {data7[0]["sum"]}
+                </Text>
+                <Text>
+                    24h: {data[0]["sum"]}
+                </Text>
+                <Text>
+                    Na papierosy
+                </Text>
             </View>
 
         )
