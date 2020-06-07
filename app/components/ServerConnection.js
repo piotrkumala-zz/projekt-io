56a7e475e8489cda6453a79541a245a9285b4a30
Patryk Chodur <patrykchodur@icloud.com>
2020-06-07 19:24:59 +0200

    Basic logging in working

--- app/components/ServerConnection.js
+++ app/components/ServerConnection.js
@@ -1,6 +1,7 @@
 
 userEmail = "";
 hostAddress = "http://192.168.1.106:3000";
+userToken = ""
 
 export const getEmail = () => {
 	// TODO fetch from server
@@ -9,7 +10,26 @@ export const getEmail = () => {
 }
 
 export const loginUser = (email, password) => {
-	userEmail = "adam@gmail.com"
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+	var urlencoded = new URLSearchParams();
+	urlencoded.append("email", email); //"antonina@testowska.test");
+	urlencoded.append("password", password); //"321apud");
+
+	var requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: urlencoded.toString(),
+		redirect: 'follow'
+	};
+
+	fetch("http://192.168.1.106:3000/users/login", requestOptions)
+		.then(response => response.json())
+		.then(result => {
+			console.log(result);
+			userToken = result.token})
+		.catch(error => console.log('error', error));
 }
 
 export const getHost = () => {
@@ -17,7 +37,7 @@ export const getHost = () => {
 }
 
 export const isUserLoggedIn = () => {
-	return userEmail !== "";
+	return userToken !== "";
 }
 
 export const registerUser = (userData) => {
