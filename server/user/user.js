3f7028846c329cd227bb129a2de421f2dcdbbbc5
Maciej Domagalski <ikslamabad@gmail.com>
2020-06-07 16:21:09 +0200

    Backend auth fix

--- server/user/user.js
+++ server/user/user.js
@@ -1,14 +1,15 @@
-var Helper = require('./helper');
 var db = require('../db');
 var pg = require('pg');
-const conString = process.env.DATABASE_URL;
-const client = new pg.Client(conString)
-
-client.connect(function(err) {
-	if(err) {
-		return console.error("could not connect to database", err);
-	}
-});
+const conString = process.env.ELEPHANTSQL_URL || "postgres://nviwmkcg:4-G6tFZ1xeDNz_W6f9C-qWQTJo8sq6Ww@drona.db.elephantsql.com:5432/nviwmkcg";
+const pool = new pg.Pool({
+    user: 'nviwmkcg',
+    host: 'drona.db.elephantsql.com',
+    database:'nviwmkcg',
+    password: '4-G6tFZ1xeDNz_W6f9C-qWQTJo8sq6Ww',
+    port: 5432,
+    ssl: true
+})
+var Helper = require('./helper');
 
 const User = {
 	async create(req, res) {
@@ -34,7 +35,7 @@ const User = {
 				req.body.gender
 			];
 		try{
-			const { rows } = await db.query(createQuery, values);
+			const { rows } = await pool.query(createQuery, values);
 			const token = Helper.generateToken(rows[0].email);
 			return res.status(201).send({ token });
 		} 
@@ -58,14 +59,16 @@ const User = {
 		}
 		const text = 'SELECT * FROM klient WHERE email = $1';
 		try{
-			const { rows } = await db.query(text, [req.body.email]);
+			const { rows } = await pool.query(text, [req.body.email]);
 			if(!rows[0]){
 				return res.status(400).send({'message': 'Nie ma takiego użytkownika'});
 			}
 			if(!Helper.comparePassword(rows[0].haslo, req.body.password)) {
 				return res.status(400).send({ 'message': 'Nieprawidłowe hasło' });
 			}
+			console.log("generate token");
 			const token = Helper.generateToken(rows[0].email);
+			console.log(token);
 			return res.status(200).send({ token });
 		}
 		catch(error){
@@ -76,7 +79,7 @@ const User = {
 	async delete(req,res){
 		const deleteQuery = 'DELETE FROM klient WHERE email=$1 returning *';
 		try{
-			const { rows } = await db.query(deleteQuery, [req.user.email]);
+			const { rows } = await pool.query(deleteQuery, [req.user.email]);
 			if(!rows[0]){
 				return res.status(404).send({'message': 'Nie znaleziono użytkownika'});
 			}
